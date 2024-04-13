02e28315d770fe156f45aefb83995aa5104099b7
AnnAgeeva <anka-287@yandex.ru>
2024-04-13 22:55:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Anna Ageeva";
+            byte age = 36;
+            string email = "anka-287@rambler.ru";
+            double programscore = 98.2;
+            double mathcsore = 80;
+            double phisicscore = 78.6;
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(age);
+            Console.WriteLine(email);
+            Console.WriteLine(programscore);
+            Console.WriteLine(mathcsore);
+            Console.WriteLine(phisicscore);
+            Console.ReadLine();
+
+            double summ = programscore + mathcsore + phisicscore;
+            double average = summ / 3;
+            Console.WriteLine(average);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
